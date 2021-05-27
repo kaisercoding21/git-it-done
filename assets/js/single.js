f577c76338b66fd21c8ff12605a35f32230d6b96
Kaisercoding <kaisercoding@outlook.com>
2021-05-27 15:29:00 -0400

    added sort-by-language functionality

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,7 +1,22 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
-var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
 
 var getRepoIssues = function (repo) {
   // format the github api url
@@ -20,23 +35,10 @@ var getRepoIssues = function (repo) {
         }
       });
     } else {
+      // if not successful, redirect to homepage
       document.location.replace("./index.html");
     }
   });
-};
-
-var getRepoName = function () {
-  var queryString = document.location.search;
-  var repoName = queryString.split("=")[1];
-
-  if (repoName) {
-    repoNameEl.textContent = repoName;
-    getRepoIssues(repoName);
-  } else {
-    document.location.replace("./index.html");
-  }
-
-  repoNameEl.textContent = repoName;
 };
 
 var displayIssues = function (issues) {
@@ -92,5 +94,4 @@ var displayWarning = function (repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues();
 getRepoName();
